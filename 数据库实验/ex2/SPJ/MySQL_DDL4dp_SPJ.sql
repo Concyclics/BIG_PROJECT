c2f228c3a4bcacfce9d4812580f6634839c44c88
Concyclics <33325023+Concyclics@users.noreply.github.com>
2021-04-18 01:05:43 +0800

    update triggers

--- 数据库实验/ex2/SPJ/MySQL_DDL4dp_SPJ.sql
+++ 数据库实验/ex2/SPJ/MySQL_DDL4dp_SPJ.sql
@@ -5,7 +5,7 @@
 #供应商表
 create table S
 (
-	SNO varchar(2) not null,
+	SNO varchar(10) not null,
 	SNAME varchar(10) unique,
 	STATUS int,
 	CITY varchar(10),
@@ -15,36 +15,43 @@ create table S
 #零件表
 create table P
 (
-	PNO varchar(2),
+	PNO varchar(20),
 	PNAME varchar(10),
 	COLOR varchar(10),
 	WEIGHT int check (WEIGHT>0),
 	primary key(PNO)
 );
 
-
 #工程项目表
 create table J
 (
-	JNO varchar(2),
+	JNO varchar(10),
 	JNAME varchar(10) default '工厂',
 	CITY varchar(10),
 	primary key(JNO)
 );
 
-
 #供应情况表
 create table SPJ
 (
-	SNO varchar(2) not null,
-	PNO varchar(2) not null,
-	JNO varchar(2) not null,
+	SNO varchar(10) default 'S1',
+	PNO varchar(10) default 'P1',
+	JNO varchar(10) default 'J1',
 	QTY int default 0,
-	primary key(SNO,PNO,JNO),
 	foreign key (SNO) references S(SNO)
-		on delete cascade,
+		on delete set NULL
+		on update cascade,
 	foreign key (PNO) references P(PNO)
-		on delete cascade,
+		on update cascade,
 	foreign key (JNO) references J(JNO)
-		on delete cascade
+		on delete no action
 );
+#MySQL 8.0不支持set default,用触发器替代
+delimiter //
+create trigger SetDefault after delete on S
+for each row
+begin
+	update SPJ set SPJ.SNO='S1' where SPJ.SNO is NULL;
+end;
+//
+delimiter ;
